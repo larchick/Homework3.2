4ab80e27432a3ef102ccd9137d5d4bbecdcf0276
larchick <liliyagreis@gmail.com>
2023-07-13 19:48:58 +0300

    cltkfyj

---
+++ src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
@@ -0,0 +1,41 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Faculty;
+
+import java.util.HashMap;
+
+@Service
+public class FacultyServiceImpl implements FacultyService {
+    private final HashMap<Long, Faculty> faculties = new HashMap<>();
+    private long count = 0;
+
+    public Faculty addFaculty(Faculty faculty){
+        faculty.setId(count++);
+        faculties.put(faculty.getId(),faculty);
+        return faculty;
+    }
+
+    public Faculty findFaculty(long id){
+        return faculties.get(id);
+    }
+
+    @Override
+    public Faculty editFaculty(long id, Faculty faculty) {
+        return null;
+    }
+
+    public Faculty editFaculty(Faculty faculty){
+        if (!faculties.containsKey(faculty.getId())){
+            return null;
+        }
+        faculties.put(faculty.getId(),faculty);
+        return faculty;
+    }
+
+    public void deleteFaculty(long id){
+        faculties.remove(id);
+    }
+
+
+}
